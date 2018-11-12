de6f4170a902cbca2a59cc6fa0af99b88d059e7c
mdolinin <mdolinin@chewy.com>
2018-11-12 18:59:53 -0500

    Initial import

---
+++ src/test/java/mdolinin/github/io/jxspringdemoapp/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package mdolinin.github.io.jxspringdemoapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
